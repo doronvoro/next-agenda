dabd7cb0abf781b5f0377ed75f940807f86fe9bb
Doron <doronvoro@gmail.com>
2025-06-15 16:08:09 +0300

    Trigger redeploy: remove search params comment

--- src/app/unauthorized/page.tsx
+++ src/app/unauthorized/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 
-export default function UnauthorizedPage() {
+export default function UnauthorizedPage() { // reomve seerch params
   const previousPath = "/";
 
   return (
